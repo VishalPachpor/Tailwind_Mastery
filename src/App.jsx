fd2526b47992cd53af1c9d2737b4fe56b3e17ec9
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-31 17:46:53 +0530

    starting css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative"> 
+      Navbar
+
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        Hero
+      </section>
+      
+      <section className="padding">
+        Popular Products 
+      </section>
+
+      <section className="padding">
+        SuperQuality
+      </section>
+
+      <section className="padding-x py-10"> 
+        Serives
+      </section>
+
+      <section className="padding">
+        SpecialOffers
+      </section>
+
+      <section className="bg-pale-blue padding">
+        CustomerReview
+      </section>
+      
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Support 
+      </section>
+
+      <section className="bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+  </main>
+)
+
+export default App
